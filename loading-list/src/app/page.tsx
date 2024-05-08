fb1c69d00c8f625eefca4d5aa2f7fb87e652209e
Gassec <vic.aublive@gmail.com>
2024-05-08 21:58:34 +0200

    21st push

---
+++ loading-list/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-around p-24 bg-slate-300">
+      {/* Loader 1 */}
+      <div className="loader"></div>
+      {/* Loader 2 */}
+      <div className="dots"></div>
+      {/* Loader 3 */}
+      <span className="loader2"></span>
+      {/* Loader 4 */}
+      <span className="loader3"></span>
+    </main>
+  );
+}
